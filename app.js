d665092275a1a7c62a92581c7c630d62fe61df99
Yuvrajraj0401 <yuvrajraj0401@gmail.com>
2025-09-05 21:49:53 +0530

    Fix: use process.env.PORT for Render

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
